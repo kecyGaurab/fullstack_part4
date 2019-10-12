e82d7c8f511299a2f405ac3477d977947c039ab9
K C Gaurab <s1900524@edu.bc.fi>
2019-10-12 12:51:42 +0300

    set up test environment and Blog list tests, step4

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,5 @@
 const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const Blog = require('../models/blog.js')
 
 blogsRouter.get('/', async (request, response) => {
 	// Blog
@@ -11,14 +11,24 @@ blogsRouter.get('/', async (request, response) => {
 	response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response) => {
-	const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+	const body = request.body
 
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: 'undefined' ? 0: body.likes,
+	}
+	)
+	
+
+	try
+	{const savedBlog = await blog.save()
+	response.json(savedBlog.toJSON())}
+	catch(exception){
+		next(exception)
+	}
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,35 +1,99 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
 
-const api = supertest(app)
+const api = supertest(app);
+const Blog = require('../models/blog');
 
-test ('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
+const initialBlogs = [
+  {
+    title: 'Music',
+    author: 'Maynard James Keenan',
+    url: 'tool.com',
+    likes: 600000,
+  },
+  {
+    title: 'Food',
+    author: 'Gordon Ramsey',
+    url: 'foodthatdontsuck.com',
+    likes: 0,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
 
 test('there are 3 blogs', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(3)
-})
+  const response = await api.get('/api/blogs');
+  expect(response.body.length).toBe(initialBlogs.length);
+});
 
-test('the author of first blog', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].author).toBe('Maynard James Keenan')
-})
+test('the author is in the blog', async () => {
+  const response = await api.get('/api/blogs');
+  const authors = response.body.map(r => r.author);
+  expect(authors).toContain('Maynard James Keenan');
+});
 
 test('the id is defined', async () => {
-    const blogs = await api.get('/api/blogs')
-    const str = JSON.stringify(blogs.body.map(blog => blog._id))
-    const id = str.replace(/\"/g,'' )
-  
-    expect(id).toBeDefined()
-})
+  const blogs = await api.get('/api/blogs');
+  const str = JSON.stringify(blogs.body.map(blog => blog._id));
+  const id = str.replace(/\"/g, '');
 
+  expect(id).toBeDefined();
+});
+
+test('a new blog list is added', async () => {
+  const newBlog = {
+    title: 'How to code',
+    author: 'Linus Trovalds',
+    url: 'https://linux.com',
+    likes: 0,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+
+  const authors = response.body.map(r => r.author);
+
+  expect(response.body.length).toBe(initialBlogs.length + 1);
+  expect(authors).toContain('Linus Trovalds');
+});
+
+test('missing like value will have 0 likes', async () => {
+  const newBlog = {
+    title: 'Origin of life',
+    author: 'Charles Darwin',
+    url: 'http://originoflife.com',
+  };
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200);
+
+  const response = await api.get('/api/blogs');
+  const index = response.body.length - 1;
+  expect(response.body[index].likes).toBe(0);
+});
 
 afterAll(() => {
-    mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
