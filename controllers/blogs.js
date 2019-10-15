01315eb6935f563f1f9ad5f69fab0e024cc37c94
K C Gaurab <s1900524@edu.bc.fi>
2019-10-15 12:21:36 +0300

    Blog list expansions, step 1 and 2

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,10 +2,23 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog.js')
 
 blogsRouter.get('/', async (request, response) => {
-
 	const blogs = await Blog.find({})
 	response.json(blogs.map(blog => blog.toJSON()))
-})
+  })
+
+
+blogsRouter.get('/:id', async (request, response, next) => {
+	try{
+		const blog = await Blog.findById(request.params.id)
+		if (blog) {
+		  response.json(blog.toJSON())
+		} else {
+		  response.status(404).end()
+		}
+	  } catch(exception) {
+		next(exception)
+	  }
+	})
 
 blogsRouter.post('/', async (request, response, next) => {
 	const body = request.body
@@ -26,6 +39,36 @@ blogsRouter.post('/', async (request, response, next) => {
 		catch(exception){
 			next(exception)
 	}
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+	try {
+	  await Blog.findByIdAndRemove(request.params.id)
+	  response.status(204).end()
+	} catch (exception) {
+	  next(exception)
+	}
+  })
+
+blogsRouter.put('/:id', async (request, response, next) => {
+	const body = request.body
+
+	const blog = {
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes
+	}
+	if(!body.title || !body.url){
+		response.status(400).send('Bad Request').end()
+	}
+try {
+	const savedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+	response.json(savedBlog.toJSON())}
+	catch(exception){
+		next(exception)
+	}
+
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,111 +1,139 @@
-const mongoose = require('mongoose');
-const supertest = require('supertest');
-const app = require('../app');
 
-const api = supertest(app);
-const Blog = require('../models/blog');
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
 
-const initialBlogs = [
-  {
-    title: 'Music',
-    author: 'Maynard James Keenan',
-    url: 'tool.com',
-    likes: 600000,
-  },
-  {
-    title: 'Food',
-    author: 'Gordon Ramsey',
-    url: 'foodthatdontsuck.com',
-    likes: 0,
-  },
-];
+const Blog = require('../models/blog')
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
 
-  let blogObject = new Blog(initialBlogs[0]);
-  await blogObject.save();
 
-  blogObject = new Blog(initialBlogs[1]);
-  await blogObject.save();
-});
+describe('when there is initially some blogs saved', () => {
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+  
+    const blogObjects = helper.initialBlogs
+      .map(blog => new Blog(blog))
+      const promiseArray = blogObjects.map(blog => blog.save())
+      await Promise.all(promiseArray)
+  })
 
-test('there are 3 blogs', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body.length).toBe(initialBlogs.length);
-});
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('the author is in the blog', async () => {
-  const response = await api.get('/api/blogs');
-  const authors = response.body.map(r => r.author);
-  expect(authors).toContain('Maynard James Keenan');
-});
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(helper.initialBlogs.length)
+  })
 
-test('the id is defined', async () => {
-  const blogs = await api.get('/api/blogs');
-  const str = JSON.stringify(blogs.body.map(blog => blog._id));
-  const id = str.replace(/\"/g, '');
+  test('the author is in the blog', async () => {
+    const response = await api.get('/api/blogs')
+    const authors = response.body.map(r => r.author)
+    expect(authors).toContain('Maynard James Keenan')
+  })
 
-  expect(id).toBeDefined();
-});
+  
+  test('the id is defined', async () => {
+    const blogs = await api.get('/api/blogs')
+    const str = JSON.stringify(blogs.body.map(blog => blog._id))
+    const id = str.replace(/\"/g, '')
 
-test('a new blog list is added', async () => {
-  const newBlog = {
-    title: 'How to code',
-    author: 'Linus Trovalds',
-    url: 'https://linux.com',
-    likes: 0,
-  };
+    expect(id).toBeDefined()
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+})
 
-  const response = await api.get('/api/blogs');
+describe('when new blog is added to the list', () =>{
+  test('a valid blog  is added', async () => {
+    const newBlog = {
+      title: 'How to code',
+      author: 'Linus Trovalds',
+      url: 'https://linux.com',
+      likes: 0,
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length+1)
+  
+    const authors = blogsAtEnd.map(r => r.author)
+  
+    expect(authors).toContain('Linus Trovalds')
+  })
 
-  const authors = response.body.map(r => r.author);
+  test('missing like value will have 0 likes', async () => {
+    const newBlog = {
+      title: 'Origin of life',
+      author: 'Charles Darwin',
+      url: 'http://originoflife.com',
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    const index = blogsAtEnd.length - 1
+    expect(blogsAtEnd[index].likes).toBe(0)
+  })
 
-  expect(response.body.length).toBe(initialBlogs.length + 1);
-  expect(authors).toContain('Linus Trovalds');
-});
-
-test('missing like value will have 0 likes', async () => {
-  const newBlog = {
-    title: 'Origin of life',
-    author: 'Charles Darwin',
-    url: 'http://originoflife.com',
-  };
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200);
-
-  const response = await api.get('/api/blogs');
-  const index = response.body.length - 1;
-  expect(response.body[index].likes).toBe(0);
-});
-
-test('throw error if title or url are missing', async () => {
+  test('throw error if title or url are missing', async () => {
     const newBlog = {
         title: 'Origin of life',
         author: 'Charles Darwin',
-     
-      };
+      }
       await api
         .post('/api/blogs')
         .send(newBlog)
-        .expect(400);
+        .expect(400)
 })
 
+})
+
+
+
+    test('a blog can be deleted', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+    
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+    
+      const blogsAtEnd = await helper.blogsInDb()
+    
+      expect(blogsAtEnd.length).toBe(
+        blogsAtStart.length - 1)
+    
+      const titles = blogsAtEnd.map(r => r.title)
+    
+      expect(titles).not.toContain(blogToDelete.title)
+    })
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
 afterAll(() => {
-  mongoose.connection.close();
-});
+  mongoose.connection.close()
+})
