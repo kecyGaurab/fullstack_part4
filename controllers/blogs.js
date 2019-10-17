1a8801e882d717a19df59261eda2bf9b65e56a9d
K C Gaurab <s1900524@edu.bc.fi>
2019-10-17 22:05:42 +0300

    bloglist expansion step 4,5 and 6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog.js')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user',{ username: 1, name : 1 })
 	response.json(blogs.map(blog => blog.toJSON()))
   })
 
@@ -22,12 +23,16 @@ blogsRouter.get('/:id', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
 	const body = request.body
+	// the information about the user who created a note is 
+	//sent in the userId field of the request body
+	const user = await User.findById(body.userId)
 
 	const blog = new Blog({
 		title: body.title,
 		author: body.author,
 		url: body.url,
 		likes: body.likes === undefined ? 0: body.likes,
+		user: user._id
 	})
 	if(!body.title || !body.url){
 		response.status(400).send('Bad Request').end()
@@ -35,6 +40,8 @@ blogsRouter.post('/', async (request, response, next) => {
 	try
 	{
 		const savedBlog = await blog.save()
+		user.blogs = user.blogs.concat(savedBlog._id)
+		await user.save()
 		response.json(savedBlog.toJSON())}
 		catch(exception){
 			next(exception)
